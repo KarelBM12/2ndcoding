0ce5d81d5f9b92794d1d599937eac969d254a323
KarelBM12 <165615816+KarelBM12@users.noreply.github.com>
2024-04-01 18:52:00 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel.Design;
+
+namespace Programming._2
+{
+    using System;
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Print numbers 1-100");
+            for (int x = 1; x <= 100; x++)
+            { 
+                if (x % 3 == 0 && x % 5 == 0)
+                {
+                    Console.WriteLine("Hello, Goodbye");
+                }
+                else if (x % 3 == 0)
+                {
+                    Console.WriteLine("Hello");
+                }
+                else if (x % 5 == 0)
+                {
+                    Console.WriteLine("Goodbye");
+                }
+                else
+                {
+                    Console.WriteLine(x);
+                }
+                
+            }
+        }
+    }
+}
